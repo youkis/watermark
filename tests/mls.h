2fb410511b94cdf9ee32ea01e00ddddc94b9f349
Youki Sada <youkis@metropolia.fi>
2017-06-01 12:59:44 +0900

    M系列ヘッダーファイルの作成と生成関数の高速化 M系列の生成関数において、原始多項式をいきなり求めていたのを 篩アルゴリズムによって、GF(2)上の既約多項式を求め、そこから 最大周期のものを求める方式に変更。コレによって大幅高速化 さらに、プリファードペアなM系列の最大の組１つを求めるプログラムを関数化

---
+++ tests/mls.h
@@ -0,0 +1,138 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int innerPro(char *x,char *y,size_t n,int t){
+	int ret=0;
+	for(int i=0;i<n;i++)ret+=x[i]*y[(i+t)%n];
+	return ret;
+}
+// GF(2)多項式の掛け算
+unsigned mul(unsigned a,unsigned b){
+	unsigned ret=0;
+	while(a!=0){
+		if(a&1) ret^=b;
+		a>>=1;
+		b<<=1;
+	}
+	return ret;
+}
+// m系列生成
+// 戻り値 1つまたは複数のm系列 系列長 (1<<n)-1
+// n: 次数
+// mls_size: 算出したmlsの個数を返す
+// is_full: 1で全てのmlsを算出 0で1本のみ
+char **mls(unsigned n,unsigned *mls_size,char is_full){
+	unsigned i,j,k;
+	unsigned N2=1<<(n+1);  // N2未満の既約多項式を求める
+	unsigned N=1<<n;
+	char *flag=(char *)malloc(sizeof(char)*N2);
+	unsigned irr_num=0;//既約多項式の数(数学的にはメビウス関数を用いて表せる)
+	unsigned mls_num=0;//m系列の数
+	unsigned *irr;     //既約多項式配列
+	char **mseqes; //m系列用二次元配列
+	char *mp; //2次元配列獲得用pointer
+
+	// 既約多項式の配列irrを篩アルゴリズムで作成
+	for(i=1;i<N2;i++) flag[i]=1;  // 全ての数に1を立てる
+	flag[1]=0;                    // 1は既約ではない
+	for(i=2;mul(i,i)<N2;i++)
+		if(flag[i]==1)
+			for(j=i;mul(i,j)<=N2;j++) flag[mul(i,j)]=0;
+	for(i=N+1;i<=N2;i++) if(flag[i]) irr_num++;
+
+	irr=malloc(sizeof(unsigned)*irr_num);
+	irr_num=0;
+	for(i=N+1;i<=N2;i++) if(flag[i]) irr[irr_num++]=i;
+	free(flag);
+
+	// 原始多項式でないirr要素は省いてirrを再構築
+	for(i=0;i<irr_num;i++){
+		unsigned f=irr[i];
+		unsigned a=N;
+		for(j=n;j<N;j++){
+			if((a&N)==N) a^=f; // N以上になったらa=a mod N
+			if(a==1) break;
+			a<<=1;
+		}
+		if(j<N-1)continue; // 周期が最大じゃなきゃ原始多項式じゃない
+		irr[mls_num++]=f;
+		if(!is_full) break;
+	}
+
+	//原始多項式からM系列生成
+	mp=(char*)malloc(sizeof(char)*mls_num*(N-1));
+	mseqes=(char**)malloc(sizeof(char*)*mls_num);
+	for(i=0;i<mls_num;i++){// fが原始多項式であるからfからM系列を求める
+		unsigned f=irr[i];
+		unsigned lfsr=1;
+		mseqes[i]=mp+i*(N-1); //mpにpointing
+		f>>=1;
+		for(k=0;k<N-1;k++){
+			lfsr=(lfsr>>1)^(-(int)(lfsr&1) & f);
+			mseqes[i][k]=(lfsr&1)*2-1;
+		}
+	}
+	free(irr);
+	//free(mseqes[0]);
+	//free(mseqes);
+	*mls_size=mls_num;
+	return mseqes;
+}
+
+char **preferd(unsigned n, unsigned *pref_size){
+	unsigned N=1<<n;
+	int i,j,k;
+	unsigned mls_size=0;
+	char **m=mls(n,&mls_size,1);
+	int mini_val1=-1;
+	int mini_val2=-1+(1<<((n+2)/2)); //プリファードペアが取るべき3つの小さい値
+	int mini_val3=-1-(1<<((n+2)/2));
+	char mat[mls_size][mls_size];
+	unsigned pair[mls_size+1],depth=0,pdepth=1;
+	char **prefm,*prefmp;
+
+	// 行列作成 要素が1である添字同士がプリファードペア
+	for(i=0;i<mls_size;i++){
+		mat[i][i]=0;
+		for(j=i+1;j<mls_size;j++){
+			char flag=1;
+			for(k=0;k<N-1;k++){
+				int inn=innerPro(m[i],m[j],N-1,k);
+				if(inn!=mini_val1 && inn!=mini_val2 && inn!=mini_val3){
+					flag=0;
+					break;
+				}
+			}
+			mat[i][j]=flag;
+			mat[j][i]=flag;
+		}
+	}
+	// 行列探索
+	pair[depth]=0;
+	while(pdepth!=depth){
+		pdepth=depth;
+		for(i=pair[depth]+1;i<mls_size;i++){
+			if(mat[pair[depth]][i]){
+				int flag=1;
+				for(j=depth-1;j>=0;j--)
+					if(mat[pair[j]][i]!=1){
+						flag=0;
+						break;
+					}
+				if(flag) pair[++depth]=i;
+			}
+		}
+	}
+	*pref_size=depth+1;
+	prefm=(char**)malloc(sizeof(char*)*depth);
+	prefmp=(char*)malloc(sizeof(char)*depth*(N-1));
+
+	for(i=0;i<depth+1;i++){
+		prefm[i]=prefmp+i*(N-1);
+		for(j=0;j<N-1;j++) prefm[i][j]=m[pair[i]][j];
+	}
+
+	free(m[0]);
+	free(m);
+	return prefm;
+}
--- tests/prefered-pair.c
+++ tests/prefered-pair.c
@@ -1,61 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-
-int innerPro(char *x,char *y,size_t n,int t){
-	int ret=0;
-	for(int i=0;i<n;i++)ret+=x[i]*y[(i+t)%n];
-	return ret;
-}
+#include"mls.h"
 
 int main(){
-	int N=7;
-	int p=pow(2,N);
-	int i,j,k;
-	char *m[1000];
-	unsigned count=0;
-	printf("N=%d\n",N);
-	printf("size=%d\n\n",p);
-	// 原始多項式
-	for(i=1;i<p;i+=2){ // 原始多項式fの最下位ビットは必ず1なので2ずつインクリメント
-		int f=p^i;
-		int a=p;
-		for(j=N;j<p;j++){
-			if((a&p)==p) a^=f;
-			if(a==1) break;
-			a<<=1;
-		}
-		if(j==p-1){ // fが原始多項式であるからfからM系列を求める
-			int init=1;
-			int lfsr=init&(p-1);
-			m[count]=(char *)malloc(sizeof(char)*(p-1));
-			f>>=1;
-			for(k=0;k<p-1;k++){
-				lfsr=(lfsr>>1)^(-(int)(lfsr&1) & f);
-				m[count][k]=(lfsr&1)*2-1;
-			}
-			count++;
-		}
-	}
-	for(i=0;i<count;i++){
-		printf("No%2d:",i+1);
-		for(j=0;j<p-1;j++) printf("%2d,",m[i][j]);
+	unsigned pref_size;
+	unsigned n=7,N=1<<n;
+	unsigned i,j;
+	char **prefm=preferd(n,&pref_size);
+	for(i=0;i<pref_size;i++){
+		printf("%2d: ",i);
+		for(j=0;j<N-1;j++) printf("%d,",prefm[i][j]);
 		printf("\n");
 	}
-	int pre[]={-1+pow(2,(int)((N+2)/2)),-1-pow(2,(int)((N+2)/2)),-1};
-	printf("プリファードペアな系列の組\n");
-	for(i=0;i<count;i++)
-		for(j=i+1;j<count;j++){
-			char flag=1;
-			for(k=0;k<p-1;k++){
-				int inn=innerPro(m[i],m[j],p-1,k);
-				if(inn!=pre[0] && inn!=pre[1] && inn!=pre[2]){
-					flag=0;
-					break;
-				}
-			}
-			if(flag) printf("No:%2d,%2d\n",i+1,j+1);
-		}
+	free(prefm[0]);
+	free(prefm);
 	return 0;
 }
-
