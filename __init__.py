81a3ae3a1c1d01f2b7c584cb69a1e07c4c190d8f
Youki Sada <youki@kaede-tokyo.net>
2017-06-22 21:39:38 +0900

    updated file

--- __init__.py
+++ __init__.py
@@ -1,2 +1,7 @@
-from .src.myfunction import *
-from .sequence import *
+import sys
+if sys.version_info.major==2:
+    from src.myfunction import *
+    from sequence import *
+if sys.version_info.major==3:
+    from watermark.src.myfunction import *
+    from watermark.sequence import *
